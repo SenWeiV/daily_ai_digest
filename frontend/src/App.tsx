39d9a8d6c2f69ae1dc1110d2fa27de855bb564d6
SenWeiV <m13230016773@gmail.com>
2026-02-16 21:07:06 +0800

    chore(frontend): trigger pages rebuild

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './pages/Home'
-import History from './pages/History'
-import Settings from './pages/Settings'
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import History from "./pages/History";
+import Settings from "./pages/Settings";
+
+// chore: trigger GitHub Pages rebuild
 
 function App() {
   return (
@@ -13,7 +15,7 @@ function App() {
         <Route path="/settings" element={<Settings />} />
       </Routes>
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
